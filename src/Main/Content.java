0a32320defc5fb22005f2cc20cbef0d79a270434
alxye <elkinal@tonbridge-school.org>
2018-07-17 16:26:30 +0000

    The angle between the player and the origin is calculated

--- src/Main/Content.java
+++ src/Main/Content.java
@@ -3,6 +3,7 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Arc2D;
 
 /**
  * Created by alxye on 16-May-18.
@@ -10,13 +11,14 @@ import java.awt.event.*;
 
 public class Content extends JPanel implements ActionListener, KeyListener, MouseListener {
     private static Timer t;
-    public static Player player = new Player(100,100,50);
+    public static Player player = new Player(300,400,50);
 
 
     //player rope mechanic
     private static boolean anchorState = false;
     private static Anchor anchor = new Anchor(0, 0, 0, 0);
     private static int anchorRadius;
+    private static double angle = 0;
 
     public Content() {
         t = new Timer(5, this);
@@ -32,8 +34,9 @@ public class Content extends JPanel implements ActionListener, KeyListener, Mous
         super.paintComponent(g);
         g.drawRect(player.getX()-player.getSize()/2, player.getY()-player.getSize()/2, player.getSize(), player.getSize());
         if(anchorState) {
-            g.drawLine(anchor.getStartX(), anchor.getStartY(), anchor.getEndX(), anchor.getEndY());
-            g.drawOval(anchor.getEndX() - anchorRadius , anchor.getEndY() - anchorRadius , anchorRadius*2, anchorRadius*2);
+            g.drawLine(anchor.getStartX(), anchor.getStartY(), anchor.getEndX(), anchor.getEndY()); //radius line
+            g.drawLine(anchor.getStartX(), anchor.getStartY(), anchor.getEndX(), anchor.getEndY() - anchorRadius); //relative angle line
+            g.drawOval(anchor.getEndX() - anchorRadius , anchor.getEndY() - anchorRadius , anchorRadius*2, anchorRadius*2); //circle
         }
 //        System.out.println(anchor.getDistance());
     }
@@ -43,8 +46,36 @@ public class Content extends JPanel implements ActionListener, KeyListener, Mous
         //resetting the anchor starting points every frame
         anchor.setStartX(player.getX());
         anchor.setStartY(player.getY());
-        anchorRadius = (int) Math.sqrt(Math.abs(Math.pow(anchor.getStartX() - anchor.getEndX(), 2) + Math.pow(anchor.getStartY() - anchor.getEndY(), 2)));
+        //experimental code to move an object in a circle
+        /*X := originX + cos(angle)*radius;
+        Y := originY + sin(angle)*radius;*/
+/*        if(anchorState) {
+            player.setX((int) (anchor.getEndX() + Math.cos(angle) * anchorRadius));
+            player.setY((int) (anchor.getEndY() + Math.sin(angle) * anchorRadius));
+            angle += (Math.PI * 2 * anchorRadius) / 3600000;
+            System.out.println("Circumference: " + (int) (Math.PI * 2 * anchorRadius) + " Distance (radians):  " + angle*5);
+//            System.out.println("Angle: " + angle + "Circumference" + Math.PI * Math.pow(anchorRadius,2));
 
+            *//*if(angle > 6.3)
+                angle = 0;*//*
+        }   //there is an error with the angle because the angle is the distance travelled along the circumference,
+            //but the circumference varies with different circle sizes
+        //----------------*/
+/*        if(anchorState) {
+            player.setX((int)(anchor.getEndX() + anchorRadius * Math.cos(angle)));
+            player.setY((int)(anchor.getEndY() + anchorRadius * Math.sin(angle)));
+            angle+=Math.PI/180.0 * 0.1; //moves 0.1 degree / frame, or 20 degrees per second
+            //experimental code here
+            *//*double angleOffset = 0;
+            if(!(anchor.getStartX() == anchor.getEndX() || anchor.getStartY() == anchor.getEndY()))
+            {
+                angleOffset = Math.toDegrees(Math.atan2(Math.abs(anchor.getEndY() - anchor.getStartY()), Math.abs(anchor.getEndX() - anchor.getStartX())));
+            }*//*
+            *//*else {
+                angleOffset = Math.toDegrees(Math.atan2((double)(Math.abs(anchor.getEndY() - anchor.getStartY())), (double)(Math.abs(anchor.getEndX() - anchor.getStartX()))));
+            }*//*
+            System.out.println(angle);
+        }*/
         repaint();
     }
 
@@ -77,12 +108,19 @@ public class Content extends JPanel implements ActionListener, KeyListener, Mous
 
     @Override
     public void mousePressed(MouseEvent e) {
-        anchorState = true;
+        anchorState = !anchorState;
 //        anchor = new Anchor(player.getX(), e.getX(), player.getY(), e.getY());
         anchor.setStartX(player.getX());
         anchor.setEndX(e.getX());
         anchor.setStartY(player.getY());
         anchor.setEndY(e.getY());
+        //really important stuff
+        angle = Math.toDegrees(Math.atan2(anchor.getEndY() - anchor.getStartY(), anchor.getEndX() - anchor.getStartX()));
+        if(angle < 0)
+            angle = 360 - Math.abs(angle);
+        System.out.println(angle);
+        //really important stuff
+        anchorRadius = (int) Math.sqrt(Math.abs(Math.pow(anchor.getStartX() - anchor.getEndX(), 2) + Math.pow(anchor.getStartY() - anchor.getEndY(), 2)));
     }
 
     @Override
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,8 +6,8 @@ import javax.swing.*;
  * Created by alxye on 17-Jul-18.
  */
 public class Main {
-    public static int screenHeight = 755;
-    public static int screenWidth = 1280;
+    public static int screenHeight = 800;
+    public static int screenWidth = 1500;
     public Main() {
         JFrame frame = new JFrame();
         frame.setTitle("Walrus Game");
